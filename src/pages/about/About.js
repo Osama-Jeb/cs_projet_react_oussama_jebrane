6704e21863245e00aaefff6c0ee0f19db373d00e
Oussama Jebrane <oussamajebrane98@gmail.com>
2023-07-26 10:37:57 +0100

    starting Home

---
+++ src/pages/about/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./_about.scss";
+
+export const About = () => {
+  return (
+    <>
+      <h1>Hello to ABOUT</h1>
+    </>
+  );
+};
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./_home.scss";
+
+export const Home = () => {
+  return (
+    <>
+      <h1 className="text-red">Hello to HOME</h1>
+    </>
+  );
+};
